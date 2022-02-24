670f852ceb305589722a09a7ba42f3263cfef79a
Vanessa Mofor <moforvanessa@gmail.com>
2022-02-24 22:49:18 +0000

    account functions

---
+++ loans.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#maximum loan amount is £10000 per account holder
+#repayment is monthly
+#interrest rate is 2.5% apr
---
+++ savings.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#create a savings goal
+#generate interest on savings
+#qualify for help-to-buy ISA
+#create sub-accounts
